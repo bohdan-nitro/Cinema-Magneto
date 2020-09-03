e1e1822087f33290f7348142b7cffb4cc61dc435
Bohdan-Nitro <bogdantrogonsky@gmail.com>
2020-09-03 21:30:35 +0300

    second commit(PC done)

--- app/js/app.js
+++ app/js/app.js
@@ -1,5 +1,42 @@
+
 $(document).ready(function () {
-    $("button").on("click", function () {
-        alert("jok");
+
+
+    let films = new Swiper('.films .swiper-container', {
+        speed: 400,
+        direction: 'horizontal',
+        spaceBetween: 20,
+        slidesPerView: 4,
+        navigation: {
+            nextEl: '.films .swiper-button-next',
+            prevEl: '.films .swiper-button-prev',
+        },
+        breakpoints: {
+            1000: {
+                slidesPerView: 3,
+            },
+            640: {
+                slidesPerView: 1,
+            },
+        }
     });
+
+    let reviews = new Swiper('.reviews .swiper-container', {
+        speed: 400,
+        spaceBetween: 40,
+        slidesPerView: 4,
+        navigation: {
+            nextEl: '.reviews .swiper-button-next',
+            prevEl: '.reviews .swiper-button-prev',
+        },
+        breakpoints: {
+            1000: {
+                slidesPerView: 3,
+            },
+            640: {
+                slidesPerView: 1,
+            },
+        }
+    });
+
 });
