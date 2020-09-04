cfd1a7c90b458c3e7b4388ba73754e92d9562650
Bohdan-Nitro <bogdantrogonsky@gmail.com>
2020-09-04 23:48:30 +0300

    fourth commit(adaptive done)

--- app/js/app.js
+++ app/js/app.js
@@ -1,6 +1,23 @@
 
 $(document).ready(function () {
 
+    let headerNavigationSelector = '.header .header-navigation';
+    let headerBurgerBtnSelector = '.header .header-burger-btn';
+    let headerLink = ".header .nav__item";
+
+    //  Burger btn click
+    $(headerBurgerBtnSelector).on('click', function () {
+
+        $(headerNavigationSelector).addClass('active').fadeIn(300);
+
+        $(headerLink).on("click", function () {
+            $(headerNavigationSelector).removeClass("active");
+
+
+        });
+
+    });
+
 
     $('a.scrollto').click(function () {
         let elementClick = $(this).attr("href");
@@ -17,17 +34,22 @@ $(document).ready(function () {
         speed: 400,
         direction: 'horizontal',
         spaceBetween: 20,
-        slidesPerView: 4,
+        slidesPerView: 1,
+        freeMode: true,
         navigation: {
             nextEl: '.films .swiper-button-next',
             prevEl: '.films .swiper-button-prev',
         },
         breakpoints: {
-            1000: {
+            1024: {
                 slidesPerView: 3,
             },
             640: {
                 slidesPerView: 1,
+            },
+            420: {
+                slidesPerView: 1,
+                spaceBetweenSlides: 30,
             },
         }
     });
@@ -41,10 +63,22 @@ $(document).ready(function () {
             prevEl: '.reviews .swiper-button-prev',
         },
         breakpoints: {
-            1000: {
+            1200: {
                 slidesPerView: 4,
             },
+            1000: {
+                slidesPerView: 1,
+            },
+            768: {
+                slidesPerView: 2,
+            },
             640: {
+                slidesPerView: 1,
+            },
+            420: {
+                slidesPerView: 1,
+            },
+            400: {
                 slidesPerView: 1,
             },
         }
